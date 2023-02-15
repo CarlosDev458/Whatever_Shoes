af8b5bffa909976a250451c376da9d4f12da98f3
Richard Lemos <cassola.delemos@hotmail.com>
2023-02-14 23:09:27 -0300

    Atualização De Autenticação do Usuário

--- main.js
+++ main.js
@@ -1,22 +1,40 @@
+const login = [
+    { user: "dani", pass: "12" },
+    { user: "ana", pass: "12" },
+    { user: "carlos", pass: "12" }
+]
+const input = document.querySelector('#submit')
+const err = "* Erro: Verifique os dados e tente novamente"
 
-function logar() {
-
+input.onclick = () => {
     const user = document.querySelector('#data_user').value
     const pass = document.querySelector('#data_pass').value
-    const err = "* Erro: Verifique os dados e tente novamente"
+    var i = 0
 
+    while (i <= login.length) {
 
-    if (user == 'admin' && pass == 'admin') {
-        location.href = "Menu/home.html"
+        if (user == login[i].user && pass == login[i].pass) {
+            location.href = "Menu/home.html"
+        }
+        i = i + 1
+        setInterval(user_not_auth,300)
     }
-    setInterval(user_not_auth, 300);
+
+    
+
     function user_not_auth() {
         document.querySelector('#msg_error').innerHTML = err
     }
-
-
-
-
 }
+
+
+
+
+
+
+
+
+
+
 
 
